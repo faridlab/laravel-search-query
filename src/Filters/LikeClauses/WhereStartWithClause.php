e14a29855b97a4cb9df47247ef1c23a71b33e124
Farid Hidayat <e.faridhidayat@gmail.com>
2021-03-12 00:22:38 +0700

    WhereStartWithClause

---
+++ src/Filters/LikeClauses/WhereStartWithClause.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace GrammaticalQuery\FilterQueryString\Filters\LikeClauses;
+
+trait WhereStartWithClause
+{
+    private function startWith($query, $filter, $values)
+    {
+        foreach((array)$values as $value) {
+            $query->orWhere($filter, 'like', "$value%");
+        }
+        return $query;
+    }
+}
