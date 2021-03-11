971d9c2b16483e320006006cb58a706b533c647c
Farid Hidayat <e.faridhidayat@gmail.com>
2021-03-12 00:22:10 +0700

    WhereEndWithClause

---
+++ src/Filters/LikeClauses/WhereEndWithClause.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace GrammaticalQuery\FilterQueryString\Filters\LikeClauses;
+
+trait WhereEndWithClause
+{
+    private function endWith($query, $filter, $values)
+    {
+        foreach((array)$values as $value) {
+            $query->orWhere($filter, 'like', "%$value");
+        }
+        return $query;
+    }
+}
