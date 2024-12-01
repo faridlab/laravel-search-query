5bcd35cdd9ce7722e838c6246b92040522e254c2
Farid Hidayat <farid@startapp.id>
2024-12-01 16:01:02 +0700

    feat: realtionship searching custom fields

--- src/Filters/RelationshipClause.php
+++ src/Filters/RelationshipClause.php
@@ -9,8 +9,25 @@ class RelationshipClause extends BaseClause {
     protected function apply($query): Builder
     {
         if(is_array($this->values)) {
-            foreach ((array) $this->values as $relation) {
-                $query->with([$relation => function($query) {
+            foreach ((array) $this->values as $relation => $value) {
+                if(!is_numeric($relation)) {
+                    // relationship[showcases]=search-value -> default field ID
+                    // relationship[showcases][field]=search-value
+                    $query->with([$relation => function($query) use($value) {
+                        if(is_array($value)) {
+                            foreach ((array) $value as $filter => $search) {
+                                $query->where($filter, $search);
+                            }
+                            $query->withTrashed();
+                            return;
+                        }
+                        $filter = 'id';
+                        $query->where($filter, $value)->withTrashed();
+                    }]);
+                    continue;
+                }
+
+                $query->with([$value => function($query) {
                     $query->withTrashed();
                 }]);
             }
